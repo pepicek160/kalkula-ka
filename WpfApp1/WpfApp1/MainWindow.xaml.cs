54712ee231326c83d981fae2d4560dfded484e83
Josef Budinský <budinskyj22@spstrutnov.cz>
2024-01-17 14:06:14 +0100

    shadowbutton style a button

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,25 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace YourNamespace
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private bool isNewInput = true;
+        private double currentResult = 0;
+        private string currentOperator = "";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +17,80 @@ namespace WpfApp1
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            Button button = (Button)sender;
+            string content = button.Content.ToString();
 
+            switch (content)
+            {
+                case "=":
+                    Calculate();
+                    break;
+                case "C":
+                    Clear();
+                    break;
+                default:
+                    AppendInput(content);
+                    break;
+            }
+        }
+
+        private void Calculate()
+        {
+            try
+            {
+                double inputValue = Convert.ToDouble(Displej.Text);
+                switch (currentOperator)
+                {
+                    case "+":
+                        currentResult += inputValue;
+                        break;
+                    case "-":
+                        currentResult -= inputValue;
+                        break;
+                    case "×":
+                        currentResult *= inputValue;
+                        break;
+                    case "÷":
+                        if (inputValue != 0)
+                            currentResult /= inputValue;
+                        else
+                            MessageBox.Show("Cannot divide by zero", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                        break;
+                    default:
+                        currentResult = inputValue;
+                        break;
+                }
+
+                Displej.Text = currentResult.ToString();
+                isNewInput = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Clear()
+        {
+            Displej.Text = "0";
+            isNewInput = true;
+            currentResult = 0;
+            currentOperator = "";
+        }
+
+        private void AppendInput(string input)
+        {
+            if (isNewInput)
+            {
+                Displej.Text = "0";
+                isNewInput = false;
+            }
+
+            if (Displej.Text == "0" && input != ",")
+                Displej.Text = input;
+            else
+                Displej.Text += input;
         }
     }
 }
+
